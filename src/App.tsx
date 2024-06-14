232eb89aaf729a9702decf08a3fec41eca88dc21
sugarfixx <ingar.torsrud@me.com>
2024-06-14 12:21:25 +0200

    Add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const currentUrl = window.location.href;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Current URL</h1>
+      <p>{currentUrl}</p>
     </div>
   );
 }
